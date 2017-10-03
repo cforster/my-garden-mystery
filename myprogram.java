02c66f88480a666a20e8c4d183b5cba488725d42
tom armstrong <tarmstrong@dalton.org>
2017-10-02 23:01:49 -0400

    the celery stalks at midnight

---
+++ myprogram.java
@@ -0,0 +1,8 @@
+public class myprogram {
+  public static void main(String[] args) {
+    String output = "_jiWXeea"; 
+    for(char c : output.toCharArray()) {
+      System.out.print((char)(c+10));
+    }
+  }
+}
