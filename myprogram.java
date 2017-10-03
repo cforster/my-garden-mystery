fb644729df47ad3b9024f048c9d66a50187e98e0
charlie <cforster@dalton.org>
2017-10-02 23:05:18 -0400

    look at the screen shot on the desktop

--- myprogram.java
+++
@@ -1,8 +0,0 @@
-public class myprogram {
-  public static void main(String[] args) {
-    String output = "_jiWXeea"; 
-    for(char c : output.toCharArray()) {
-      System.out.print((char)(c+10));
-    }
-  }
-}
